3d3d65151731ec9a129daad3f1ee39717ffeb822
Chong <monkey.d.chong@gmail.com>
2021-07-05 15:08:16 +0700

    Ep2 - Display

---
+++ Display.java
@@ -0,0 +1,12 @@
+/**
+ * Display
+ */
+public class Display {
+    public static void main(String[] args) {
+        // แสดงผลแบบขึ้นบรรทัดใหม่
+        System.out.println("Display on Print line");
+
+        // แสดงผลแบบไม่ขึ้นบรรทัดใหม่
+        System.out.print("Display on Print");
+    }
+}
